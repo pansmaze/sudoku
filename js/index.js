2628a047dfdf12788a74b41765372cf571c419e3
周扬竣 <pansmaze@gmail.com>
2015-01-22 19:33:59 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 ! function() {
     var mySudoku = new Sudoku({
-        bSize: 30,
-        bBColor: '#000',
-        gBColor: '#CCC',
-        labelColor: '#000',
-        textColor: '#777',
-        etextColor: '#E02727',
-        container: $('#J-sudoku_stage'),
-        puzzle_placeholder: '0',
-        puzzle: '985702134267134580314890762832976451076453208549218673621087945403629807790541326',
+        bSize: 30,// 每个block的宽高
+        bBColor: '#000',// 外层边框颜色
+        gBColor: '#CCC',//宫细边框颜色
+        labelColor: '#000',// 题目数字文字颜色
+        textColor: '#777',// 填入内容文字颜色
+        etextColor: '#E02727',// 出错文字颜色
+        container: $('#J-sudoku_stage'),// 容器
+        puzzle_placeholder: '0',// 谜面占位符
+        puzzle: PUZZLE,
         answer: '985762134267134589314895762832976451176453298549218673621387945453629817798541326',
         error: function(msg) {
             if (msg) {
